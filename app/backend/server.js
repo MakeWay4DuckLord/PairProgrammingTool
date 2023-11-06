03a5597f98ad46a4fbf27c37ed09d08fa078e59a
sjstone3 <sjstone3@ncsu.edu>
2023-11-06 09:56:19 -0500

    Added cors policy to express server

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const db = require('./config/db.js');
 
+app.use(cors())
+
 // Import and use the db api routes
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(443, () => {
+  console.log('Server is running on port 443');
 });
