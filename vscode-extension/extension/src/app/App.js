2b32e54f6393287956b6574741b32da1cb76d175
Lindsey Dunkley <lfdunkle@ncsu.edu>
2023-11-06 07:41:47 -0500

    Updated extension with link + pages. Just need WS.

--- vscode-extension/extension/src/app/App.js
+++ vscode-extension/extension/src/app/App.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Session from './components/Session'
 import EndSession from './components/EndSession'
+import styles from './css/App.module.css'
 
 const App = () => {
-  const [page, setPage] = React.useState('start');
+  const [page, setPage] = React.useState('');
   return (
-    <div>
+    <div className={styles.App}>
+      {page === '' && <a href="http://localhost:3000/"><button>Create New Session</button></a>}
       {page === 'start' && <Session onSwitch={setPage}/>}
       {page === 'end' && <EndSession onSwitch={setPage} />}
     </div>
