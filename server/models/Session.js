27099dbd0379c0bb71e609ff5fd84bee267c3399
sjstone3 <sjstone3@ncsu.edu>
2023-11-13 11:18:37 -0500

    Trying to update branch removing old server files

--- server/models/Session.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const sessionSchema = new Schema({
-    user1_id: String,
-    user2_id: String
-});
-
-module.exports = mongoose.model("Session", sessionSchema);
